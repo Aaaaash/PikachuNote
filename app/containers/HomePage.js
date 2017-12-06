2a7be2a2502fb4d6d9e26111f91adbf728da5e11
sakub <sakura@labixiaoxiudeMacBook-Pro.local>
2017-12-06 15:32:45 +0800

    fix package

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,12 +1,21 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import { TitleBar, Toolbar } from 'react-desktop/macOs';
 import '@blueprintjs/core/dist/blueprint.css';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default class HomePage extends PureComponent {
   render() {
     return (
-      <div style={{ color: '#000' }}>
-        <h1>test</h1>
-      </div>
+      <Container>
+        <TitleBar controls inset>
+          <Toolbar height="43" horizontalAlignment="center" />
+        </TitleBar>
+      </Container>
     );
   }
 }
