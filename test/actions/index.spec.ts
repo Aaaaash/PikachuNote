9a18d8ab5c256189fb5e09d24e9bcb13375a1feb
sakuraash <841105127@qq.com>
2018-01-16 11:34:50 +0800

    update reducers test

--- test/actions/index.spec.ts
+++ test/actions/index.spec.ts
@@ -2,6 +2,10 @@ import * as actions from '../../app/actions';
 
 describe('actions', () => {
   it('should fetchAllData should create fetchAllData action', () => {
-    expect(actions.fetchAllData('dbName', 'storeName'));
+    expect(actions.fetchAllData('dbName', 'storeName')).toMatchSnapshot();
+  });
+
+  it('should fetchAllDataComplete should create fetchAllDataComplete action', () => {
+    expect(actions.fetchAllDataComplete([])).toMatchSnapshot();
   });
 });
--- test/reducers/index.spec.ts
+++ test/reducers/index.spec.ts
@@ -1,16 +1,38 @@
 import reducer from '../../app/reducers/sidebarReducer';
 import * as actions from '../../app/actions';
+import { Directory } from '../../app/types';
+
+const MOCK_INITIAL_STATE = {
+  dir: [],
+  dirDetails: [],
+  currentDir: '',
+  active: '',
+  currentNote: {},
+};
+
+const MOCK_DIRECTORY_LIST: Array<Directory> = [
+  {
+    id: 'aaa',
+    title: 'test',
+    children: [],
+    belong: 'xxx',
+    type: 'NOTE',
+    createTime: Number(new Date()) / 1000,
+    lastUpdateTime: Number(new Date()) / 1000,
+    tags: ['jest', 'ts'],
+  }
+];
 
 describe('reducers', () => {
-  describe('counter', () => {
+  describe('initial State', () => {
     it('should handle initial state', () => {
-      expect(reducer(undefined, { type: 'unknown' })).toEqual({
-        dir: [],
-        dirDetails: [],
-        currentDir: '',
-        active: '',
-        currentNote: {},
+      expect(reducer(MOCK_INITIAL_STATE, { type: 'unknown' })).toEqual(MOCK_INITIAL_STATE);
+    });
+    it('should handle FETCH_ALL_DIRS_COMPLETE action', () => {
+      expect(reducer(MOCK_INITIAL_STATE, actions.fetchAllDataComplete(MOCK_DIRECTORY_LIST))).toEqual({
+        ...MOCK_INITIAL_STATE,
+        dir: MOCK_DIRECTORY_LIST,
       });
     })
-  })
-})
+  });
+});
